e238d8d6cb603144e4673a48bdcd7f3efeb0c722
narin116 <aasdff1010@gmail.com>
2026-06-01 16:49:53 +0900

    feat: app.js에 sale Controller 연결

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import saleController from './controllers/saleController';
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ['http://localhost:3000'];
 
 app.use(
   cors({
@@ -14,21 +15,23 @@ app.use(
         return;
       }
 
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     },
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Favorite Photo Backend API" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Favorite Photo Backend API' });
 });
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
 });
+
+app.use('/sales', saleController);
 
 export default app;
---
+++ src/controllers/saleController.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const saleController = express.Router();
+
+export default saleController;
