176c6fa246608f97f6afe190f7785a8465172d6f
narin116 <aasdff1010@gmail.com>
2026-06-01 17:01:45 +0900

    feat: sale Router 코드 작성

--- src/routes/saleRoutes.js
+++ src/routes/saleRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import saleController from '../controllers/saleController.js';
 
 const router = express.Router();
+
+router.post(saleController.createSale); //카드 판매 등록
+router.patch('/:saleId', saleController.modifySale); //판매 정보 수정
+router.delete('/:saleId', saleController.deleteSale); //판매 취소
 
 export default router;
