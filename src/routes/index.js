f95297e462a788627744a482891ad3910dc429b6
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-06-02 18:36:13 +0900

    refactor: gallery 라우트 구조 정리

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes.js';
 import exchangeRoutes from './exchange.routes.js';
+import galleryRoutes from './gallery.route.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/', exchangeRoutes);
+router.use('/', galleryRoutes);
 
 export default router;
