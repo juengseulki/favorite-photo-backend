b573dc36e86279ad88a1e9bf5ee8a046c309303a
DevGangMin <fire0609@naver.com>
2026-06-10 11:37:42 +0900

    feat: 포토카드 상세 페이지 및 구매 기능 구현

--- src/routes/market.route.js
+++ src/routes/market.route.js
@@ -4,11 +4,12 @@ import {
   getMarketCardDetail,
   purchaseCards,
 } from '../controllers/market.controller.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.get('/cards', getMarketCards);
 router.get('/cards/:saleId', getMarketCardDetail);
-router.post('/cards/:saleId/purchase', purchaseCards);
+router.post('/cards/:saleId/purchase', authenticate, purchaseCards);
 
 export default router;
