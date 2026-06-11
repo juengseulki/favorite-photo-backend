510bfe21d41ee47ccca0ffc67fc0a40fa65c74b1
narin116 <aasdff1010@gmail.com>
2026-06-11 13:57:34 +0900

    feat: sale의 APIs, 구매 API에 인증 미들웨어 (authenticate) 추가

--- src/routes/market.route.js
+++ src/routes/market.route.js
@@ -4,11 +4,12 @@ import {
   getMarketCardDetail,
   purchaseCards,
 } from '../controllers/market.controller.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.get('/cards', getMarketCards);
 router.get('/cards/:saleId', getMarketCardDetail);
-router.post('/cards/:saleId/purchase', purchaseCards);
+router.post('/cards/:saleId/purchase', authenticate, purchaseCards); //구매 API. 로그인 된 사용자만 접근할 수 있도록 함(authenticate)
 
 export default router;
