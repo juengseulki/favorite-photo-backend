058b34db706e6de664f4602f1156d5f6996a52c7
narin116 <aasdff1010@gmail.com>
2026-06-11 18:05:27 +0900

    feat: tradesService Sale카드 포맷팅까지 완료

--- src/routes/gallery.route.js
+++ src/routes/gallery.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getMyCards, postMyCards } from '../controllers/gallery.controller.js';
+import {
+  getMyCards,
+  postMyCards,
+  getMyTrades,
+} from '../controllers/gallery.controller.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.get('/me/cards', authenticate, getMyCards);
 router.post('/me/cards', authenticate, postMyCards);
+router.get('/me/sales/cards', authenticate, getMyTrades);
 
 export default router;
